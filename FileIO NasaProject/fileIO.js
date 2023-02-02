7bdddb2e9ea43f528dac0c8c2e88cd5ececaf6f1
Bohdan <bogdantelep@gmail.com>
2023-02-02 22:51:53 +0300

    reading .csv file

--- FileIO NasaProject/fileIO.js
+++ FileIO NasaProject/fileIO.js
@@ -1,1 +1,23 @@
 const { parse } = require('csv-parse');
+const fs = require('fs');
+
+// main
+const results = [];
+
+// creating readStream for our csv-parser
+fs.createReadStream('kepler_data.csv')
+    // readable.pipe(writeable)
+    // converting from bytes to normal
+    .pipe(parse({
+        columns: true,
+        comment: '#',
+    }))
+    .on('data', (chunk) => {
+        results.push(chunk);
+    })
+    .on('error', (error) => {
+        console.log(error);
+    })
+    .on('end', () => {
+        console.log(results);
+    })
