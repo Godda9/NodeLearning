6a5bc8645837e3906fd535d13afb5dba4189f1e9
Bohdan <bogdantelep@gmail.com>
2023-02-05 17:23:07 +0300

    Posting data

---
+++ HttpServers Basics/ParameterizedURLs.js
@@ -0,0 +1,37 @@
+const http = require('http');
+
+const data = [
+    {
+        id: 0,
+        text: 'abc0',
+    },
+    {
+        id: 1,
+        text: 'abc1',
+    },
+    {
+        id: 2,
+        text: 'abc2',
+    }
+]
+
+const PORT = 3000;
+const SERVER = http.createServer((req, res) => {
+    const items = req.url.split('/');
+    if (items[1] === 'data') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        if (items.length === 3) {
+            res.end(JSON.stringify(data[+items[2]]));
+        } else {
+            res.end(JSON.stringify(data));
+        }
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+});
+
+SERVER.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
--- HttpServers Basics/WebServerWithRoutes.js
+++ HttpServers Basics/WebServerWithRoutes.js
@@ -3,26 +3,16 @@ const http = require('http');
 const PORT = 3000;
 const SERVER = http.createServer((req, res) => {
     if (req.url === '/test0') {
-        //res.writeHead(200, {
-        //    'Content-Type': 'application/json',
-        //});
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({
-            id: 0,
+            id: 1,
             text: 'abc',
         }));
     } else if (req.url === '/test1') {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html')
-        res.write(`
-            <html>
-                <body>
-                    <h2>Html text</h2>
-                </body>
-            </html>
-        `);
-        res.end();
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h2>Html text</h2</body></html');
     } else {
         res.statusCode = 404;
         res.end();
@@ -30,5 +20,5 @@ const SERVER = http.createServer((req, res) => {
 });
 
 SERVER.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 })
