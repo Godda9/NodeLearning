27224262d514957f96804e9b8ef311b8148d1db6
Bohdan <bogdantelep@gmail.com>
2023-02-10 16:29:51 +0300

    connecting react and node server

--- NASA Proj/server/src/app.js
+++ NASA Proj/server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const planetsRouter = require('./routers/planets/planets.router');
 
 
@@ -17,9 +18,13 @@ app.use((req, res, next) => {
     const delta = new Date() - start;
     console.log(`${req.method} ${req.baseUrl} ${req.url} ${delta}ms`);
 });
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // MAIN
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 // EXPORT
 module.exports = app;
