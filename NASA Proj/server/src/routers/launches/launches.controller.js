ca1294898e5cd62e70b5ed62f62d43619fbc7d82
Bohdan <bogdantelep@gmail.com>
2023-02-11 18:57:49 -0800

    validation

--- NASA Proj/server/src/routers/launches/launches.controller.js
+++ NASA Proj/server/src/routers/launches/launches.controller.js
@@ -7,9 +7,26 @@ const getAllLaunches = (req, res) => {
 
 const addNewLaunch = (req, res) => {
     const launch = req.body;
-    launch.launchDate = new Date(launch.launchDate);
+
+    // check 1
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+        return res.status(400).json({
+            error: 'Missing important argument',
+        });
+    }
+
+    // set date
+    launch.launchDate = new Date(launch.launchDate); 
+
+    // check 2
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid datetime',
+        });
+    }
+
+    // add
     launchesModel.addNewLaunch(launch);
-    return res.status(201).json(launch);
 }
 
 // EXPORT
